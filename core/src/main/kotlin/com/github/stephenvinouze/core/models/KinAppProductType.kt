d518544e17b084b94057398932d0b2654eef273a
Pieterjan Vandegaer <pieterjan.vandegaer@captaintouch.be>
2022-10-06 22:14:37 +0200

    update to v5 billing

--- core/src/main/kotlin/com/github/stephenvinouze/core/models/KinAppProductType.kt
+++ core/src/main/kotlin/com/github/stephenvinouze/core/models/KinAppProductType.kt
@@ -1,10 +1,19 @@
 package com.github.stephenvinouze.core.models
 
+import com.android.billingclient.api.BillingClient
+import com.android.billingclient.api.BillingResult
 import com.github.stephenvinouze.core.managers.KinAppManager
 
 /**
  * Created by stephenvinouze on 10/05/2017.
  */
 enum class KinAppProductType(val value: String) {
-    INAPP(KinAppManager.INAPP_TYPE), SUBSCRIPTION(KinAppManager.SUBS_TYPE)
+    INAPP(KinAppManager.INAPP_TYPE), SUBSCRIPTION(KinAppManager.SUBS_TYPE);
+
+    fun billingType(): String {
+        when (this) {
+            INAPP -> return BillingClient.SkuType.INAPP
+            SUBSCRIPTION -> return BillingClient.SkuType.SUBS
+        }
+    }
 }
